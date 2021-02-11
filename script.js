262f741cba6ca3c4624f8b31a32408512447217c
Denis V <vladudenis09@gmail.com>
2021-02-11 11:19:09 +0100

    This is the very basic logic of the calculator.

---
+++ script.js
@@ -0,0 +1,28 @@
+function operate(operator, a, b){
+    switch(operator){
+        case "+": 
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return  a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
