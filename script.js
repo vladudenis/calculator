4238e5f2825ca6dc2d1628c10cd5ef5bdd2fa891
Denis V <vladudenis09@gmail.com>
2021-02-12 17:34:42 +0100

    Added functionality and button hover/toggle effects. I'll hunt for bugs and update the project with a batch of bugfixes to finish it all off.

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
+let op = "";
+let temp = 0;
+
+const displayData = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
+let buttonsArr = Array.from(buttons);
+buttonsArr.forEach(button => btnAddListener(button));
+
+function btnAddListener(btn){
+    if(btn.className == "number-btn"){
+        btn.addEventListener("click", numberBtn);
+    }
+    else if(btn.className == "operator-btn"){
+        btn.addEventListener("click", operatorBtn);
+    }
+    else if(btn.className == "dot-btn"){
+        btn.addEventListener("click", dotBtn);
+    }
+    else if(btn.className == "eval-btn"){
+        btn.addEventListener("click", evalBtn);
+    }
+    else if(btn.className == "del-clear-btn clear"){
+        btn.addEventListener("click", clearBtn);
+    }
+    else if(btn.className == "del-clear-btn del"){
+        btn.addEventListener("click", deleteBtn);
+    }
+}
+
+function numberBtn(e){
+    if((op == "" && displayData.textContent != 0) || displayData.textContent.includes(".")){
+        displayData.textContent += e.target.textContent;
+    }
+    else{
+       displayData.textContent = e.target.textContent;
+    } 
+}
+
+function operatorBtn(e){
+    if(op == ""){
+        op = String(e.target.textContent);
+        temp = displayData.textContent;
+    }
+}
+
+function dotBtn(e){
+    if(!displayData.textContent.includes(".")){
+        displayData.textContent += e.target.textContent;
+    }
+}
+
+function evalBtn(e){
+    if(op != ""){
+        displayData.textContent = operate(op, temp, displayData.textContent);
+        op = "";
+        temp = 0;
+    }
+}
+
+function clearBtn(){
+    displayData.textContent = 0;
+    op = "";
+    temp = 0;
+}
+
+function deleteBtn(){
+    displayData.textContent =  displayData.textContent.toString().slice(0, -1);
+}
+
+//change these to make sure they also work with decimals
 function operate(operator, a, b){
+    a = Number(a);
+    b = Number(b);
     switch(operator){
         case "+": 
             return add(a, b);
@@ -6,7 +78,7 @@ function operate(operator, a, b){
             return subtract(a, b);
         case "*":
             return multiply(a, b);
-        case "/":
+        case "÷":
             return divide(a, b);
     }
 }
@@ -24,5 +96,6 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if(b == 0) return "Error";
     return a / b;
 }
